3a3fe17530dc20ecdd1c39b8a4eec6b87a5ff7b8
Filip Jonsson <filip.jonsson@mildef.com>
2023-12-04 07:55:49 +0100

    modified:   Day 18/painting/main.py 	new file:   Day 18/painting/my_image.jpg

--- Day 18/painting/main.py
+++ Day 18/painting/main.py
@@ -1,2 +1,8 @@
 import colorgram
-colors = colorgram.extract("my_image.jpg", 30)
+colors = colorgram.extract("C:\\Users\\filjon\\OneDrive - Mildef\\Dokument\\Code\\100DaysOfCode\\Day 18\\painting\\my_image.jpg", 30)
+
+rgp_colors = []
+for color in colors:
+    rgp_colors.append((color.rgb.r, color.rgb.g, color.rgb.b))
+
+print(rgp_colors)
