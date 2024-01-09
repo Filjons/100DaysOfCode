4c1a0e835f07a6ab6fb23b1cbabc9bd531bf198d
Filip Jonsson <filip.jonsson@mildef.com>
2024-01-09 08:04:50 +0100

    Changes to be committed: 	modified:   Day 22/ball.py 	modified:   Day 22/main.py

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -1,7 +1,8 @@
 from turtle import Turtle, xcor, ycor
 
-X_LIMIT = 250
-Y_LIMIT = 250
+X_LIMIT = 270
+
+
 
 
 class Ball(Turtle):
@@ -11,20 +12,36 @@ class Ball(Turtle):
         self.up()
         self.shape('square')
         self.color('white')
+        self.shapesize(1.0, 1.0)
         self.move_x = 10
         self.move_y = 10
 
     def move(self):
-        new_x = xcor() + self.move_x
-        new_y = ycor() + self.move_y
+        new_x = self.xcor() + self.move_x
+        new_y = self.ycor() + self.move_y
         self.goto(new_x, new_y)
+
+    def bounce_x(self):
+       
+        self.move_x *= -1
 
     def bounce_y(self):
        
-        self.move_y = -1 * self.move_y
+        self.move_y *= -1
 
-    def collision(self):
+    def wall_collision(self, y_limit):
        
-        if self.ycor() > Y_LIMIT or self.ycor() < -Y_LIMIT:
+        if self.ycor() > y_limit or self.ycor() < -y_limit:
             self.bounce_y()
-        
+    
+    def padel_collision(self, x_limit, padel_distance):
+        print(padel_distance)
+        if x_limit > 0 and self.xcor() > x_limit :
+            print(self.xcor())
+            if padel_distance < 10:
+                print("padel hit")
+                self.bounce_x()
+        elif x_limit < 0 and self.xcor() < x_limit:
+            print(self.xcor())
+            if padel_distance < 10:
+                self.bounce_x()
--- Day 22/main.py
+++ Day 22/main.py
@@ -6,6 +6,11 @@ from padel import Padel
 from divider import Divider
 from scoreboard import Scoreboard
 from ball import Ball
+
+Y_LIMIT = 270
+X_LIMIT = 250
+
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -13,8 +18,8 @@ screen.title("Pong")
 screen.tracer(0)
 
 # Padels
-padel_one = Padel((270, 0))
-padel_two = Padel((-270, 0))
+padel_r = Padel((270, 0))
+padel_l = Padel((-270, 0))
 
 # Ball
 ball = Ball()
@@ -29,14 +34,18 @@ score_two.refresh_score()
 net = Divider()
 
 screen.listen()
-screen.onkeypress(padel_one.go_up, "o")
-screen.onkeypress(padel_one.go_down, "l")
-screen.onkeypress(padel_two.go_up, "q")
-screen.onkeypress(padel_two.go_down, "a")
+screen.onkeypress(padel_r.go_up, "o")
+screen.onkeypress(padel_r.go_down, "l")
+screen.onkeypress(padel_l.go_up, "q")
+screen.onkeypress(padel_l.go_down, "a")
 
 game_on = True
 while game_on:
     sleep(0.1)
     screen.update()
     ball.move()
-    ball.collision()
+    ball.wall_collision(y_limit=Y_LIMIT)
+    ball.padel_collision(x_limit=X_LIMIT, padel_distance=ball.distance(padel_r))
+    ball.padel_collision(x_limit=-X_LIMIT, padel_distance=ball.distance(padel_l))
+
+
