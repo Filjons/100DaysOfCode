9f2090f225b4bf12c2489e86d90005073247f01a
Filip Jonsson <filip.jonsson@mildef.com>
2024-01-10 08:04:07 +0100

    modified:   Day 22/ball.py 	modified:   Day 22/main.py

--- Day 22/ball.py
+++ Day 22/ball.py
@@ -1,13 +1,9 @@
 from turtle import Turtle, xcor, ycor
 
-X_LIMIT = 270
-
-
-
 
 class Ball(Turtle):
 
-    def __init__(self):
+    def __init__(self, x_limit, y_limit):
         super().__init__()
         self.up()
         self.shape('square')
@@ -15,11 +11,18 @@ class Ball(Turtle):
         self.shapesize(1.0, 1.0)
         self.move_x = 10
         self.move_y = 10
+        self.x_limit = x_limit
+        self.y_limit = y_limit
 
     def move(self):
         new_x = self.xcor() + self.move_x
         new_y = self.ycor() + self.move_y
         self.goto(new_x, new_y)
+        if new_x > 0 and new_x > self.x_limit:
+            self.reset()
+        elif new_x < 0 and new_x < -self.x_limit:
+            self.reset()    
+        
 
     def bounce_x(self):
        
@@ -28,20 +31,24 @@ class Ball(Turtle):
     def bounce_y(self):
        
         self.move_y *= -1
+    
+    def ball_reset(self):
+        self.bounce_x()
+        self.goto(0,0)
 
-    def wall_collision(self, y_limit):
+    def wall_collision(self):
        
-        if self.ycor() > y_limit or self.ycor() < -y_limit:
+        if self.ycor() > self.y_limit:
             self.bounce_y()
     
-    def padel_collision(self, x_limit, padel_distance):
-        print(padel_distance)
-        if x_limit > 0 and self.xcor() > x_limit :
+    def padel_collision(self, padel_limit, padel_distance):
+        
+        if padel_limit > 0 and self.xcor() > padel_limit :
             print(self.xcor())
-            if padel_distance < 10:
+            if padel_distance < 200:
                 print("padel hit")
                 self.bounce_x()
-        elif x_limit < 0 and self.xcor() < x_limit:
+        elif padel_limit < 0 and self.xcor() < padel_limit:
             print(self.xcor())
-            if padel_distance < 10:
+            if padel_distance < 200:
                 self.bounce_x()
--- Day 22/main.py
+++ Day 22/main.py
@@ -8,8 +8,8 @@ from scoreboard import Scoreboard
 from ball import Ball
 
 Y_LIMIT = 270
-X_LIMIT = 250
-
+X_LIMIT = 270
+PADEL_LIMIT = X_LIMIT - 20
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -22,13 +22,13 @@ padel_r = Padel((270, 0))
 padel_l = Padel((-270, 0))
 
 # Ball
-ball = Ball()
+ball = Ball(x_limit=X_LIMIT, y_limit=Y_LIMIT)
 
 # Scoreboards
-score_one = Scoreboard((30, 260))
-score_one.refresh_score()
-score_two = Scoreboard((-30, 260))
-score_two.refresh_score()
+score_l = Scoreboard((30, 260))
+score_l.refresh_score()
+score_r = Scoreboard((-30, 260))
+score_r.refresh_score()
 
 # Divider
 net = Divider()
@@ -45,7 +45,10 @@ while game_on:
     screen.update()
     ball.move()
     ball.wall_collision(y_limit=Y_LIMIT)
-    ball.padel_collision(x_limit=X_LIMIT, padel_distance=ball.distance(padel_r))
-    ball.padel_collision(x_limit=-X_LIMIT, padel_distance=ball.distance(padel_l))
-
+    ball.padel_collision(padel_limit=PADEL_LIMIT, padel_distance=ball.distance(padel_r))
+    if ball.padel_miss(x_limit=X_LIMIT):
+        score_r.new_score()
+    ball.padel_collision(padel_limit=-PADEL_LIMIT, padel_distance=ball.distance(padel_l))
+    if ball.padel_miss(x_limit=-X_LIMIT):
+        score_l.new_score()
 
