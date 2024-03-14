d6bd9746cc0765c416c5eb0202e4eb90c542dbe0
Filip Jonsson <filip.jonsson@mildef.com>
2024-03-14 07:54:11 +0100

    started with exception handeling on day 30 Changes to be committed: 	modified:   Day 29/main.py 	new file:   Day 29/password_file.txt 	deleted:    password_file.txt

--- Day 29/main.py
+++ Day 29/main.py
@@ -21,13 +21,14 @@ def save_password():
     website = website_input.get()
     username = username_input.get()
     password = password_input.get()
-
-    if messagebox.askokcancel(title="Save Information",message=f"Are these details correct? \nWebsite:{website}\nUsername:{username}\nPassword:{password}"):
+    if website == "" or username == "" or password == "":
+       messagebox.askretrycancel(title="Missing Information", message="Please enter all information.") 
+    elif messagebox.askokcancel(title="Save Information",message=f"Is this information correct? \nWebsite: {website}\nUsername: {username}\nPassword: {password}"):
         with open("password_file.txt", "a") as pf:
             pf.write(f"{website} | {username} | {password}\n")
     
-    website_input.delete(0,END)
-    password_input.delete(0,END)
+        website_input.delete(0,END)
+        password_input.delete(0,END)
 
 #----- UI SETUP -----
 window = Tk()
