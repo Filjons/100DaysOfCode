ea06382b38e85a9b99c66226d09e2fe12230a6aa
Filip Jonsson <filip.jonsson@mildef.com>
2024-04-05 07:55:40 +0200

    made flashcard to show words  Changes to be committed: 	modified:   Day 31/main.py

--- Day 31/main.py
+++ Day 31/main.py
@@ -1,8 +1,23 @@
 
 from tkinter import *
 from tkinter import messagebox
+import pandas as pd
 
 BACKGROUND_COLOR = "#B1DDC6"
+DATA_FILE = "Day 31\\data\\french_words.csv"
+
+word = "Word"
+# ----- DICTIONARIE -----
+data_file = pd.read_csv(DATA_FILE)
+
+word_list = data_file.to_dict()
+
+print(word_list)
+
+def flip_card():
+    global word
+    word = "word 2"
+    flash_card.itemconfig(card_word, text=word)
 
 # ----- UI SETUP -----
 window = Tk()
@@ -14,19 +29,23 @@ right_image = PhotoImage(file="Day 31\\images\\right.png")
 wrong_image = PhotoImage(file="Day 31\\images\\wrong.png")
 front_card = PhotoImage(file="Day 31\\images\\card_front.png")
 back_card = PhotoImage(file="Day 31\\images\\card_back.png")
+cards = {front_card,back_card}
 
 # ----- FLASH CARD -----
 flash_card = Canvas(height=526, width=800,
                     highlightthickness=0, bg=BACKGROUND_COLOR)
 flash_card.create_image(400, 263, image=front_card)
+flash_card.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+card_word = flash_card.create_text(400, 263, text=word, font=("Ariel", 60, "bold"))
+
 flash_card.grid(row=0, column=0, columnspan=2)
 
 # ----- BUTTONS -----
-right_button = Button(image=right_image, highlightthickness=0)
-right_button.grid(row=1, column=0)
-
 wrong_button = Button(image=wrong_image, highlightthickness=0)
-wrong_button.grid(row=1, column=1)
+wrong_button.grid(row=1, column=0)
+
+right_button = Button(image=right_image, highlightthickness=0, command=flip_card)
+right_button.grid(row=1, column=1)
 
 # ----- LABELS -----
 # website = Label(text="Website")
