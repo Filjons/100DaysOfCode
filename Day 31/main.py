f1150916116207854dbef24adb6801d781e76e62
Filip Jonsson <filip.jonsson@mildef.com>
2024-04-18 08:02:54 +0200

    fixed the flip timer  Changes to be committed: 	modified:   Day 31/main.py

--- Day 31/main.py
+++ Day 31/main.py
@@ -6,7 +6,7 @@ import random as rd
 BACKGROUND_COLOR = "#B1DDC6"
 DATA_FILE = "Day 31\\data\\french_words.csv"
 
-word = "Word"
+
 word_list = {}
 
 # ----- DICTIONARY -----
@@ -14,16 +14,28 @@ data_file = pd.read_csv(DATA_FILE)
 
 word_list = data_file.to_dict(orient='records')
 
+word = rd.choice(word_list)
+
 def next_card():
-    global word
+    global word, flip_timer
+    window.after_cancel(flip_timer)
     word = rd.choice(word_list)
-    flash_card.itemconfig(card_title, text="French")
-    flash_card.itemconfig(card_word, text=word["French"])
+    flash_card.itemconfig(card_title, text="French", fill="black")
+    flash_card.itemconfig(card_word, text=word["French"], fill="black")
+    flash_card.itemconfig(card_background, image=front_card)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    flash_card.itemconfig(card_title, text="English", fill="white")
+    flash_card.itemconfig(card_word, text=word["English"], fill="white")
+    flash_card.itemconfig(card_background, image=back_card)
 
 # ----- UI SETUP -----
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+flip_timer = window.after(3000, func=flip_card)
 
 # ----- IMAGE SETUP -----
 right_image = PhotoImage(file="Day 31\\images\\right.png")
@@ -35,9 +47,11 @@ cards = {front_card,back_card}
 # ----- FLASH CARD -----
 flash_card = Canvas(height=526, width=800,
                     highlightthickness=0, bg=BACKGROUND_COLOR)
-flash_card.create_image(400, 263, image=front_card)
-card_title = flash_card.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-card_word = flash_card.create_text(400, 263, text=word, font=("Ariel", 60, "bold"))
+card_background = flash_card.create_image(400, 263, image=front_card)
+card_title = flash_card.create_text(
+    400, 150, text="French", font=("Ariel", 40, "italic"))
+card_word = flash_card.create_text(
+    400, 263, text=word["French"], font=("Ariel", 60, "bold"))
 
 flash_card.grid(row=0, column=0, columnspan=2)
 
