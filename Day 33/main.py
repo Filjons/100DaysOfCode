a5d3543947e9100a138bae64acf4225420ad6c5e
Filip Jonsson <filip.jonsson@mildef.com>
2024-05-15 08:17:46 +0200

    made check position function  Changes to be committed: 	modified:   Day 33/main.py 	deleted:    Day 33/test.py

--- Day 33/main.py
+++ Day 33/main.py
@@ -1,15 +1,28 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 51.507351  # Your latitude
-MY_LONG = -0.127758  # Your longitude
+MY_LAT = 56.07428613231186  # Your latitude
+MY_LONG = 12.71759061741751  # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
-data = response.json()
+iss_data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+iss_latitude = float(iss_data["iss_position"]["latitude"])
+iss_longitude = float(iss_data["iss_position"]["longitude"])
+#iss_latitude = float(53)
+#iss_longitude = float(10)
+
+
+def iss_position():
+
+    if iss_latitude < (MY_LAT + 5.0) and iss_latitude > (MY_LAT - 5.0):
+        if iss_longitude < (MY_LONG + 5.0) and iss_longitude > (MY_LONG - 5.0):
+            return True
+        else:
+            return False
+    else:
+        return False
 
 # Your position is within +5 or -5 degrees of the ISS position.
 
@@ -28,7 +41,11 @@ sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
-print(data)
+result = data["results"]
+sunrise = result["sunrise"]
+sunset = result["sunset"]
+print(f"sunrise: {sunrise} and sunset: {sunset}")
+print(iss_position())
 # If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
