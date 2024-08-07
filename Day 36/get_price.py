5b71a3fe8af5d7aaca0f89efb8125f84d9d41a2d
Filip Jonsson <filip.jonsson@mildef.com>
2024-08-07 08:17:01 +0200

    Changes to be committed: 	modified:   Day 36/get_price.py 	new file:   json_file.json

--- Day 36/get_price.py
+++ Day 36/get_price.py
@@ -1,29 +1,31 @@
 import requests
+import json
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 
 API_FUNCTION = "TIME_SERIES_DAILY"
+'''
+Get the price data API call
+replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
+'''
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-#url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
 r = requests.get(url)
 data = r.json()
 
-print(data)
-with open("json_file.json", "w") as data_file:
-    json.dump(new_data, data_file, indent=4)
+# Retrive the closing price for yesterday and the day before
+key_Z = 'Time Series (Daily)'
+keys = data[key_Z].keys()
 
-with open("json_file.json", "r") as data_file:
-    # Reading old data
 
-    data = dict(json.load(data_file))
-    print(type(data))
-    print(data)
-    # Update old data
-    data.update(newer_data)
-with open("json_file.json", "w") as data_file:
-    # Save updated data
-    json.dump(data, data_file, indent=4)
+key_A = list(keys)[0]
+key_B = list(keys)[1]
+
+price_A = float(data[key_Z][key_A]['4. close'])
+price_B = float(data[key_Z][key_B]['4. close'])
+
+diff = abs(price_A - price_B)
+print(diff/price_A)
