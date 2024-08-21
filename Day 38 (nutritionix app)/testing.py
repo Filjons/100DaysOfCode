e01813940b18634f128602505888186511e4f064
Filip Jonsson <filip.jonsson@mildef.com>
2024-08-21 07:51:34 +0200

    Changes to be committed: 	modified:   Day 38 (nutritionix app)/testing.py

--- Day 38 (nutritionix app)/testing.py
+++ Day 38 (nutritionix app)/testing.py
@@ -3,14 +3,15 @@ from get_stuff import get_stuff
 
 
 USER_NAME = "tamari-sauce"
-PROJ_NAME = "My_workouts"
+PROJ_NAME = "myWorkouts"
 SHEET_NAME = "workouts"
 SHEETY_EP = f"https://api.sheety.co/{USER_NAME}/{PROJ_NAME}/{SHEET_NAME}"
 
 
-def sheety_get_row(row=int):
+def sheety_get_row(row=2):
     credentials = get_stuff('sheety')
     header = {
+        "Content-Type": "application/json",
         "Authorization": f"Bearer {credentials['token']}"
     }
     row_url = f"{SHEETY_EP}/{row}"
@@ -21,4 +22,5 @@ def sheety_get_row(row=int):
 if __name__ == "__main__":
 
     r = sheety_get_row()
+    print(r.json())
     print(r.text)
