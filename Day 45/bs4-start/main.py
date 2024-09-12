ea63fb2ca6feb881ac4972732d2b4b1f1eaf35e1
Tamari21 <filipjons@outlook.com>
2024-09-12 20:18:21 +0200

    modified:   Day 45/bs4-start/main.py

--- Day 45/bs4-start/main.py
+++ Day 45/bs4-start/main.py
@@ -4,6 +4,18 @@ with open("Day 45\\bs4-start\\website.html") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, "html.parser")
-
+'''
 #how to get all tags?
-print(soup.find_all(name="a"))
+all_tags = soup.find_all(name="a")
+
+for tag in all_tags:
+    print(tag.text)
+
+section_heading = soup.find_all(name="h3", class_="heading")
+for tag in section_heading:
+    print(tag.text)
+
+'''
+company_url = soup.select_one(selector="p")
+
+print(company_url)
