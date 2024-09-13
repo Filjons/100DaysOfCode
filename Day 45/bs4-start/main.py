2b0db32633dd507df1319db04278743c29de1b74
Filip Jonsson <filip.jonsson@mildef.com>
2024-09-13 08:10:52 +0200

    Changes to be committed: 	modified:   Day 45/bs4-start/main.py

--- Day 45/bs4-start/main.py
+++ Day 45/bs4-start/main.py
@@ -1,10 +1,35 @@
 from bs4 import BeautifulSoup
+import requests
 
+#get all data from the web page
+url = "https://news.ycombinator.com/news"
+responese = requests.get(url)
+
+web_page = responese.text
+#parse the data into a soup using the html.parser
+soup = BeautifulSoup(web_page, "html.parser")
+#print(soup.prettify())
+story_list = soup.find_all(name="span", class_="titleline",)
+
+
+for tag in story_list:
+    print(tag.text)
+
+
+
+
+
+
+
+
+
+
+'''
 with open("Day 45\\bs4-start\\website.html") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, "html.parser")
-'''
+
 #how to get all tags?
 all_tags = soup.find_all(name="a")
 
@@ -15,7 +40,7 @@ section_heading = soup.find_all(name="h3", class_="heading")
 for tag in section_heading:
     print(tag.text)
 
-'''
+
 company_url = soup.select_one(selector="p")
 
-print(company_url)
+print(company_url)'''
