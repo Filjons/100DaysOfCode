108afd75fd4ca5722fda16b7eb460de02fa7c8a0
Filip Jonsson <filip.jonsson@mildef.com>
2024-09-30 08:07:09 +0200

    modified:   Day 46(spotify project)/main.py

--- Day 46(spotify project)/main.py
+++ Day 46(spotify project)/main.py
@@ -11,19 +11,27 @@ CLIENT_SECRET = credantials["token"]
 REDIRECT_URI = "http://example.com"
 
 # Write your code below this line 👇
+
+#get user input
 #user_input = input("Which year do you want to travel to? (YYY-MM-DD): ")
 user_input = "1988-03-19"
+
+#send request to billboard
 responese = requests.get(url=f"{URL}/{user_input}")
 
-web_page = responese.text
 # parse the data into a soup using the html.parser
+
+web_page = responese.text
 soup = BeautifulSoup(web_page, "html.parser")
 #print(soup.prettify())
+
+#extract data (songs)
 #CSS selectors to drill down in the tag tree
 ranked_list = soup.select(selector="div ul li #title-of-a-story")
 #print(ranked_list)
-file_name = f"top_100_songs_{user_input}.txt"
 
+#Save data to file
+file_name = f"top_100_songs_{user_input}.txt"
 with open(file_name,"w", encoding="utf-8") as file:
     [file.write(f"{song.get_text().strip()}\n") for song in ranked_list]
 
@@ -36,7 +44,7 @@ with open(file_name, "r") as file:
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                client_secret=CLIENT_SECRET,
                                                redirect_uri=REDIRECT_URI,
-                                               scope="user-library-read"))
+                                               scope="user-library-read , playlist-modify-public"))
 
 # Test code by fetching some sweet taylor swift stuff.
 
