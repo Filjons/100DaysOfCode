c99f392e3c6ba616377d1d85c6ebc37266a59f31
Filip Jonsson <filip.jonsson@mildef.com>
2024-10-23 07:41:06 +0200

    Changes to be committed: 	new file:   Day 54(Web development w flask)/__pycache__/hello.cpython-312.pyc 	modified:   Day 54(Web development w flask)/hello.py

--- Day 54(Web development w flask)/hello.py
+++ Day 54(Web development w flask)/hello.py
@@ -1,9 +1,37 @@
 from flask import Flask
 app = Flask(__name__)
 
-#For windows systems to environmental variables
+
 #set FLASK_APP=hello.py
-#
+
+#from flask import Flask
+#app = Flask(__name__)
+
+
+#@app.route('/')
+#def hello_world():
+#   return 'Hello World’
+
+#if __name__ == '__main__':
+#   app.run()
+
+#app.run(host='127.0.0.1',port=8000,debug=True)
+
+
+# For windows systems to environmental variables
+# export FLASK_APP=app.py # your python file name
+# set FLASK_APP = app.py
+# export FLASK_ENV=development
+# set FLASK_DEBUG = True
+# python -m flask --app hello run
+
+# More info: https://flask.palletsprojects.com/en/3.0.x/quickstart/#a-minimal-application
+
+
 @app.route('/')
 def hello_world():
     return 'hello world!'
+
+
+if __name__ == '__main__':
+   app.run()
